e63ed2d4d07e23288bc6d415a8d193866fad138f
Unknown <pedrodlafuente@gmail.com>
2018-09-27 11:01:14 +0200

    -points-patch

--- db.go
+++ db.go
@@ -2,16 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/ethereum/go-ethereum/common"
 )
+
+type T4wAccount struct {
+	AUserID      string `json:"userID"`
+	InitialOints int    `json:"points"`
+	Key          string `json:"key"`
+	Address      string `json:"address"`
+	AURL         string `json: "url"`
+}
 
 var db = dynamodb.New(session.New(), aws.NewConfig().WithRegion("eu-west-1"))
 
-func putProduct(account *T4wAccount) error {
+func saveT4wAccount(account *T4wAccount) error {
 	av, err := dynamodbattribute.MarshalMap(account)
 	input := &dynamodb.PutItemInput{
 		Item:      av,
@@ -26,4 +36,31 @@ func putProduct(account *T4wAccount) error {
 		return err
 	}
 	return nil
+}
+func getUsserAddress(userID int64) (common.Address, error) {
+
+	result, err := db.GetItem(&dynamodb.GetItemInput{
+		TableName: aws.String("likedat-accounts"),
+		Key: map[string]*dynamodb.AttributeValue{
+			"userID": {
+				N: aws.String(string(userID)),
+			},
+		},
+	})
+
+	if err != nil {
+		log.Println(err.Error())
+		log.Printf("User not found-->: %v", userID)
+	}
+
+	item := T4wAccount{}
+
+	err = dynamodbattribute.UnmarshalMap(result.Item, &item)
+
+	if err != nil {
+		panic(fmt.Sprintf("Failed to unmarshal Record, %v", err))
+	}
+
+	addres := common.HexToAddress(item.Address)
+	return addres, nil
 }
